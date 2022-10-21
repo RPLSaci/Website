98aad0cdcca3ae522e9828a4439d061ee420fcd4
MoonLGH <MoonLGH@users.noreply.github.com>
2022-10-21 09:42:21 +0000

    deploy: f640fcf5d1ab722298b267af5ef947f27dd16b61

---
+++ index.8a4473b8.js
@@ -0,0 +1,2 @@
+let date=new Date;document.getElementById("FooterYear")&&(document.getElementById("FooterYear").innerText=document.getElementById("FooterYear").innerText.replace("xxxx",date.getFullYear()));
+//# sourceMappingURL=index.8a4473b8.js.map
