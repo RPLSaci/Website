3f2c7d165aa80fc32a3d69c8886423754e1a40d3
MoonLGH <MoonLMC1212@gmail.com>
2022-11-02 07:32:21 +0000

    final

--- .github/scripts/push.sh
+++ .github/scripts/push.sh
@@ -6,10 +6,6 @@ git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git status
 
-if [ -n "$(git status --porcelain)" ]; then
-    git add .
-    git commit -m "Update Website"
-    git push
-else
-    echo "No changes to commit"
-fi
+git add .
+git commit -m "Update Website"
+git push
